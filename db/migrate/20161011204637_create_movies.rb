e4c9c9b3a2a042baf8dfe8e525aa1d737ff1be79
stompdoctor <jonjpeterkin@gmail.com>
2016-10-12 00:44:26 -0400

    Done.

---
+++ db/migrate/20161011204637_create_movies.rb
@@ -0,0 +1,13 @@
+class CreateMovies < ActiveRecord::Migration
+  def change
+  	create_table :movies do |t|
+  		t.string :title
+  		t.string :director
+  		t.string :lead
+  		t.boolean :in_theaters
+  		t.integer :release_date
+
+  		t.timestamps
+  	end
+  end
+end
